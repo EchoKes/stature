c987b76dab94960ae39490a45816efefd8f0b0b0
EchoKes <iamkester1@gmail.com>
2025-07-16 14:19:55 +0800

    improve: mobile responsiveness on services

--- components/HoverCard.vue
+++ components/HoverCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="group relative w-[400px] h-[370px]">
+  <div class="group relative w-[295px] h-[273px] md:w-[400px] md:h-[370px]">
     <!-- Floating corner icon (outside the clipped area) -->
     <div
-      class="absolute top-0 right-0 z-10 w-[3rem] h-[3rem] bg-white rounded-full flex items-center justify-center group-hover:bg-[#dbeb50]"
+      class="absolute top-0 right-0 z-10 w-[2.2rem] h-[2.2rem] md:w-[3rem] md:h-[3rem] bg-white rounded-full flex items-center justify-center group-hover:bg-[#dbeb50]"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -22,17 +22,17 @@
 
     <!-- card background -->
     <div
-      class="card-services relative bg-white flex flex-col justify-between text-left w-full h-full overflow-hidden z-10 before:absolute before:content-[''] before:top-[-67%] before:right-[35%] before:w-full before:h-full before:bg-[#dbeb50] before:-rotate-45 before:origin-top-right before:scale-x-0 before:scale-y-[200%] before:transition-transform before:duration-[600ms] group-hover:before:scale-x-[200%]"
+      class="card-services-tiny md:card-services-default relative bg-white flex flex-col justify-between text-left w-full h-full overflow-hidden z-10 before:absolute before:content-[''] before:top-[-67%] before:right-[35%] before:w-full before:h-full before:bg-[#dbeb50] before:-rotate-45 before:origin-top-right before:scale-x-0 before:scale-y-[200%] before:transition-transform before:duration-[600ms] group-hover:before:scale-x-[200%]"
     >
-      <div class="text-3xl font-semibold leading-tight m-6 z-10">
+      <div class="text-3xl font-semibold leading-tight m-6 mb-0 z-10">
         {{ title }}
       </div>
 
-      <div class="w-full overflow-hidden flex items-center">
+      <div class="w-full overflow-hidden flex items-center pt-2">
         <img
           :src="img"
           alt="Service Image"
-          class="object-contain w-full h-auto transition-transform duration-300 translate-y-1 group-hover:-translate-y-1"
+          class="object-contain w-full h-auto transition-transform duration-300 translate-y-3 group-hover:-translate-y-0"
         />
       </div>
     </div>
